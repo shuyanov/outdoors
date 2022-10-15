276723f00b972fbd5f43cfd339bf1a5a01d8f684
sweet62 <maks_shuyanov@mail.ru>
2022-10-15 18:49:05 +0300

    Add FirstProject
    new

---
+++ lib/model/weatherModel.dart
@@ -0,0 +1,47 @@
+class Weather{
+  DateTime dateTime;
+  num degree;
+  String iconUrl;
+  int clouds;
+
+  Weather({
+    required this.dateTime,
+    required this.degree,
+    required this.iconUrl,
+    required this.clouds
+  });
+
+  static const String weatherURL ="http://openweathermap.org/img/wn/";
+
+  String getIconURL() => "$weatherURL$iconUrl.png";
+}
+
+class DayHeading{
+  final DateTime dateTime;
+
+  DayHeading({
+    required this.dateTime
+  });
+}
+
+class Placemark{
+  final double lat;
+  final double lon;
+  final String cityName;
+  final int timeStamp = DateTime.now().millisecondsSinceEpoch;
+
+  Placemark({
+    required this.lat,
+    required this.lon,
+    required this.cityName,
+  });
+
+  Map<String, dynamic> getPlacemark(){
+    return{
+      "lat": this.lat,
+      "lon": this.lon,
+      "cityName": this.cityName,
+      "timestamp": this.timeStamp
+    };
+  }
+}
